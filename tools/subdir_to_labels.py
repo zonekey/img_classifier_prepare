5ab32fbdc956032d03ad44161ea7d06d864e2f1a
sunkw <sunkwei@gmail.com>
2016-06-07 15:24:49 +0800

    首先读取 labels0.txt 中的标签，然后搜索工作目录中额外的子目录作为新的标签，一起生成 labels.txt 中

--- tools/subdir_to_labels.py
+++ tools/subdir_to_labels.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 #
 # @file: subdir_to_labels.py
-# @date: 2016-06-01
+# @date: 2016-06-07 如果labels0.txt 已经存在，则不修改 labels0 中对应的 labels 文件的顺序...
 # @brief:
 # @detail:
 #
@@ -13,17 +13,32 @@ import os, sys
 ''' 根据当前目录下的子目录，生成 labels.txt 文件 '''
 
 ignored = [ 'skipped', 'confused' ]
+catalogs = []
 
+# labels0.txt 中对应着模型训练的类别 ...
+try:
+    with open('labels0.txt', 'r') as f:
+        for line in f:
+            c = line.strip()
+            if len(c) > 1:
+                catalogs.append(c)
+except:
+    pass
+print 'orig catalogs:', len(catalogs)
 
+# 工作目录中，自动添加非 labels0 中的子目录作为标签 ...
 working_dir = "./"
 if len(sys.argv) == 2:
     working_dir = sys.argv[1]
 
-label = open('labels.txt', 'w')
-
 cnt = 0
+label = open('labels.txt', 'w')
+for c in catalogs:
+    label.write(c + '\n')
+    cnt += 1
+
 for fn in os.listdir(working_dir):
-    if os.path.isdir(working_dir + '/' + fn) and fn not in ignored:
+    if os.path.isdir(working_dir + '/' + fn) and fn not in ignored and fn not in catalogs:
         label.write(fn + '\n')
         cnt += 1
 
