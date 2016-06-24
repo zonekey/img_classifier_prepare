ac3e06f6f1fc29c261ec1144a0997b664349f756
liuguangyuan <liuguangyuan0532@gmail.com>
2016-06-24 15:01:01 +0800

    ..

--- fine_tune/fine_train.py
+++ fine_tune/fine_train.py
@@ -2,6 +2,14 @@ import dbhlp
 import random
 import subprocess
 import sys
+import sqlite3 as sq
+import threading
+
+def get_database_record(fname):
+    conn = sq.connect(fname)
+    cmd = 'select fname , label from img where label >= 0'
+    cursor = self.__conn.execute(cmd)
+    return  cursor.fetchall()
 
 def change_solver(val):
     with open('solver0.prototxt', 'r') as fp:
@@ -9,7 +17,7 @@ def change_solver(val):
     k, v = kvs[1].split(":")
     kvs[1] = k + ' ' + str(val) + ' \n'
 
-    with open('solver0.prototxt', 'w') as fp:
+    with open('solver.prototxt', 'w') as fp:
         fp.writelines(kvs)
 
 def get_txt(datas, fname):
@@ -22,8 +30,7 @@ def get_txt(datas, fname):
 
 
 if __name__ == '__main__':
-    db = dbhlp.DB('labels.db')
-    imagelist = db.get_imgs_labels()
+    imagelist = get_database_records('labels.db')
     random.shuffle(imagelist)
     
     length = len(imagelist) * 4 / 5
