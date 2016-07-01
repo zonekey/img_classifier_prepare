bfbbca6b947e70620ab1085313409c4b25550081
sunkw <sunkwei@gmail.com>
2016-07-01 15:13:41 +0800

    训练完成，通知到 tk1

--- web/training.py
+++ web/training.py
@@ -40,6 +40,8 @@ class TrainApiHandler(BaseRequest):
             return self.stop_train()
         if cmd == 'get_progress':
             return self.get_progress()
+        if cmd == 'update':
+            return self.update()
 
         self.finish('unknown cmd:' + cmd)
 
@@ -146,12 +148,26 @@ class TrainApiHandler(BaseRequest):
         self.finish(rx)
 
 
+    def update(self):
+        ''' /train/api/update?ip=xxx '''
+        ip = self.get_query_argument('ip')
+        self.do_update(ip)
+        rx = { 'status': 'ok' }
+        self.finish(rx)
+
+
     def get_elapsed(self, info):
         ''' 根据 iter_num 和 time 估算完成 20000 次迭代需要的时间'''
         if info['iter_num'] > 0:
             return 20000.0 * (info['time'] / info['iter_num'])
         else:
             return -1.0
+
+
+    def do_update(self, ip):
+        ''' TODO: 实现上传 ... '''
+        print 'do_update, ip:', ip
+        pass
 
 
 
