19d27834644e084b55dbd967ed67959f43df461b
sunkw <sunkwei@gmail.com>
2016-07-12 13:22:41 +0800

    get ip

--- web/zip_send.sh
+++ web/zip_send.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 #rm -f ../models/pretrained.caffemodel
-mv ../models/pretrained.caffemodel training/tmp-_iter_1000.caffemodel
-cp  training/tmp-_iter_1000.caffemodel ../models/pretrained.caffemodel
+mv ../models/pretrained.caffemodel training/tmp-_iter_${1}.caffemodel
+cp  training/tmp-_iter_${1}.caffemodel ../models/pretrained.caffemodel
+
+ip= ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'
+
 zip -o pub/m.zip ../models/deploy.prototxt  ../models/labels.txt  ../models/mean.binaryproto  ../models/pretrained.caffemodel   
-curl -X PUT -d '{"url": "http://172.16.1.60:8890/pub/m.zip}' "http://${2}:18000/train/api/update?func=start_download"
+curl -X PUT -d '{"url": "http://${ip}:8889/pub/m.zip}' "http://${2}:18000/train/api/update?func=start_download"
