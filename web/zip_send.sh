20d0087869f0410f76c9f31c904883ea76e337cc
liuguangyuan <liugy@zonekey.com>
2016-07-12 13:49:11 +0800

    ..

--- web/zip_send.sh
+++ web/zip_send.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cp  training/tmp-_iter_${1}.caffemodel ../models/pretrained.caffemodel
-ip= ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'
 
 zip -o pub/m.zip ../models/deploy.prototxt  ../models/labels.txt  ../models/mean.binaryproto  ../models/pretrained.caffemodel   
+
+ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 curl -X PUT -d '{"url": "http://${ip}:8889/pub/m.zip}' "http://${2}:18000/train/api/update?func=start_download"
